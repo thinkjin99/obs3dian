340a3ec6987acbc3875641765450932ffcf26ec8
정명진 <thinkjin99@gmail.com>
2024-03-05 20:37:51 +0900

    Add descriotion in markdown

--- src/obs3dian/markdown.py
+++ src/obs3dian/markdown.py
@@ -8,8 +8,13 @@ from urllib.parse import unquote
 
 @dataclass
 class ImageText:
+    """
+    Data container for image.
+    This class contains image file name, path and line number in markdown.
+    """
+
     name: str
-    line: int
+    line_no: int
     path: Path
     metadata: str | None = None
     s3_url: str | None = None
@@ -22,7 +27,7 @@ def _extract_image_from_no_path_patt(match_result: re.Match) -> dict:
     return image_info
 
 
-def _extrace_image_from_path_patt(match_result: re.Match) -> dict | None:
+def _extract_image_from_path_patt(match_result: re.Match) -> dict | None:
     image_info = match_result.groupdict()
     image_path = image_info.get("path")
     assert image_path, "ImageText doesn't have path"
@@ -37,37 +42,58 @@ def _extrace_image_from_path_patt(match_result: re.Match) -> dict | None:
         raise ValueError(f"{image_path} is invalid path")
 
 
+def _extract_image_by_patt(
+    func_patt_map: List[Tuple[Callable, str]],
+    name_path_map: dict[str, Path],
+    line: str,
+    line_no: int,
+) -> List[dict]:
+
+    image_infos: List[dict] = []
+    for func, patt in func_patt_map:
+        if match_results := re.finditer(patt, line):  # match pattern
+            for match_result in match_results:
+                if image_info := func(match_result):  # run match func for patt
+                    try:
+                        image_info["path"] = name_path_map[image_info["name"]]
+                        image_info["line_no"] = line_no
+                        image_infos.append(image_info)
+                    except KeyError:  # image file could be not exists
+                        continue
+
+    return image_infos
+
+
 def extract_images_from_md(
     markdown_file_path: Path, name_path_map: dict[str, Path]
 ) -> List[ImageText]:
     """
-    Get imageText names from .md file
+    Extract images from md file
 
     Args:
-        markdown_file_path (Path): md file path
+        markdown_file_path (Path): markdown file path
+        name_path_map (dict[str, Path]): image name path map {abc.png : /foo/abc.png}
 
     Returns:
-        List[str]: imageText names in md file
+        List[ImageText]: Image data in markdown
     """
     no_path_patt = r"!\[\[(?P<name>[^]]+\.(png|jpg|jpeg|gif))\|?(?P<metadata>[^]]+)?\]\]"  # group 1 = file_name, group 2 = foramt, group3 = imageText metadata
     path_patt = r"!\[(?P<metadata>[^]]+)\]\((?P<path>[^)]+\.png|jpg|jpeg|gif\))"  # group 1 = metadata group 2 = file path
 
     func_patt_map: List[Tuple[Callable, str]] = [
         (_extract_image_from_no_path_patt, no_path_patt),
-        (_extrace_image_from_path_patt, path_patt),
+        (_extract_image_from_path_patt, path_patt),
     ]  # regex patt and match function
 
     images: List[ImageText] = []
     with markdown_file_path.open("r") as f:
         for line_no, line in enumerate(f):  # read lines
-            for func, patt in func_patt_map:
-                if match_results := re.finditer(patt, line):  # match pattern
-                    for match_result in match_results:
-                        if image_info := func(match_result):
-                            image_info["path"] = name_path_map[image_info["name"]]
-                            images.append(
-                                ImageText(**image_info, line=line_no)
-                            )  # append imageText data
+            image_infos = _extract_image_by_patt(
+                func_patt_map, name_path_map, line, line_no
+            )
+            images = [ImageText(**image_info) for image_info in image_infos]
+            images.extend(images)
+
     return images
 
 
@@ -109,8 +135,8 @@ def write_md_file(
         with open(out_file_path, "w") as output_file:
             for line_no, line in enumerate(origin_file):
                 for image in uploaded_images:
-                    if line_no == image.line:
-                        line = f"![{image.metadata}]({image.s3_url})"
+                    if line_no == image.line_no:
+                        line = f"![{image.metadata}]({image.s3_url})\n"
                 output_file.write(line)  # if no replace just copy line
 
     if is_overwrite:
--- src/test/test_run.py
+++ src/test/test_run.py
@@ -4,14 +4,12 @@ from obs3dian.main import run
 
 
 class TestRun:
-    user_input_path = pathlib.Path("./test_files")
+    user_input_path = pathlib.Path("/Users/jin/Programming/project/obs3dian/test_files")
 
     def test_by_profile_run(self):
         # test by profile
-        run(user_input_path=self.user_input_path, overwrite=False, useprofile=True)
+        run(user_input_path=self.user_input_path, overwrite=False, usekey=False)
 
     def test_by_keys(self):
         # test with access key
-        run(user_input_path=self.user_input_path, overwrite=False, useprofile=False)
-
-    pass
+        run(user_input_path=self.user_input_path, overwrite=False, usekey=True)
